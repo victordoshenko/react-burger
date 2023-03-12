0675d526c09bee9a3a23c1f1710de691733008c8
Victor Doshchenko <vdoschenko@vtb.ru>
2023-03-12 17:02:12 +0300

    sprint-1/step-2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react';
+import StyleApp from './app.module.css'
+import { AppHeader } from "../app-header/app-header";
+import { BurgerConstructor } from "../burger-constructor/burger-constructor";
+import {BurgerIngredients} from "../burger-ingredients/burger-ingredients";
+
+const UrlDomain = "https://norma.nomoreparties.space";
+
+export const App = () => {
+
+     const [state, setState] = useState({
+         error: false,
+         loading: false,
+         data: []
+     });
+
+     useEffect(() => {
+         const getData = () => {
+             setState({ ...state, error: false, loading: true });
+             fetch(UrlDomain + "/api/ingredients")
+             .then((res) => {return res.json();})
+                 .then((res) => {
+                     setState({ ...state, data: res.data, loading: false });
+                 })
+                 .catch((err) => {
+                     alert(err);
+                     setState({ ...state, error: true, loading: false });
+                 });
+         };
+
+         getData();
+     }, []);
+
+  return (
+    <div className={StyleApp.app}>
+        <AppHeader />
+        <div className={StyleApp.mainConstructor}>
+            <BurgerIngredients products={ state.data }/>
+            <BurgerConstructor ingredients={ state.data }/>
+        </div>
+    </div>
+  );
+}
