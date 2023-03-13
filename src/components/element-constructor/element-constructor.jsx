91d1ab62eeed51935e55e45313e3a6962834831f
Victor Doshchenko <vdoschenko@vtb.ru>
2023-03-13 12:52:55 +0300

    Code Review fixes: part 4: buns

--- src/components/element-constructor/element-constructor.jsx
+++ src/components/element-constructor/element-constructor.jsx
@@ -1,5 +1,4 @@
 import {ConstructorElement} from "@ya.praktikum/react-developer-burger-ui-components";
-import PropTypes from "prop-types";
 import React from "react";
 import StyleElement from "./element-constructor.module.css"
 
@@ -10,7 +9,7 @@ export const ElementConstructor = ({ingredients}) => {
             <div className={`${StyleElement.element}`}>
                 <ConstructorElement
                     type={type}
-                    isLocked={true}
+                    isLocked={type === "bun" ? false : true}
                     text={name}
                     price={price}
                     thumbnail={thumbnail}
@@ -19,45 +18,29 @@ export const ElementConstructor = ({ingredients}) => {
         )
     }
 
-    Element.propTypes = {
-        name: PropTypes.string,
-        thumbnail: PropTypes.string,
-        price: PropTypes.number,
-        type: PropTypes.oneOf(["top" | "bottom" | undefined]),
-        isLocked: PropTypes.oneOf([PropTypes.bool | undefined]),
-        extraClass: PropTypes.oneOf([PropTypes.string | undefined]),
-        handleClose: PropTypes.func,
-    }
-
     const arrBun = [];
     const arrMain = [];
 
     ingredients.map((ingredient) => {
-        if (ingredient.name === "Краторная булка N-200i") {
+        if (ingredient.type === "bun") {
             arrBun.push(ingredient);
-        }
-        if (ingredient.type === "sauce" || ingredient.type === "main") {
+        } else {
             arrMain.push(ingredient);
             arrMain.splice(3, 2);
         }
         return ingredient;
-    })
+    });
 
     return (
         <>
             {
                 arrBun.map((ingredient) => (
-                    <ElementBuild type="top" price={ingredient.price} name={ingredient.name} thumbnail={ingredient.image} key={ingredient._id}/>
+                    <ElementBuild type={ingredient.type} price={ingredient.price} name={ingredient.name} thumbnail={ingredient.image} key={ingredient._id}/>
                 ))
             }
             {
                 arrMain.map((ingredient) => (
                     <ElementBuild price={ingredient.price} name={ingredient.name} thumbnail={ingredient.image} key={ingredient._id}/>
-                ))
-            }
-            {
-                arrBun.map((ingredient) => (
-                    <ElementBuild type="bottom" price={ingredient.price} name={ingredient.name} thumbnail={ingredient.image} key={ingredient._id}/>
                 ))
             }
         </>
